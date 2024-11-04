e80d0125bd1a3dfbf1bea68840a52f94340722fb
NeekOwO <viethailongvuong@gmail.com>
2024-11-04 10:06:57 +0700

    Update database connection string to use MongoDB Atlas

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/nodejs-api-test", {
+    await mongoose.connect("mongodb+srv://ahhmin:12200409@node-api-test.60bxn.mongodb.net/?retryWrites=true&w=majority&appName=node-api-test", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
